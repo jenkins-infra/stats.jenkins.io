4f04086b9e2cbb8b59f9e3dc34cbcd0c6d7050e0
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-05-22 19:10:34 -0400

    add menu to navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Box, Button, Typography, styled } from '@mui/material'
+import React, { useState } from 'react'
+import { Box, Button, Typography, styled, Menu, MenuItem } from '@mui/material'
 import { FaGithub } from 'react-icons/fa'
-import { Link } from 'react-router-dom' // Import Link from react-router-dom
+import { Link } from 'react-router-dom'
+import { KeyboardArrowDown } from '@mui/icons-material'
 import theme from '../theme/theme'
 
 const StyledBar = styled(Box)(({ theme }) => ({
@@ -34,35 +35,81 @@ const StyledName = styled(Typography)(({ theme }) => ({
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = () => {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
     return (
         <StyledBar>
             <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
                 <StyledName variant="h6">Jenkins</StyledName>
             </Link>
-            <Button
-                variant="contained"
-                startIcon={<FaGithub />}
-                href="https://github.com/shlomomdahan/stats2.jenkins.io"
-                sx={{
-                    margin: '0.5rem',
-                    backgroundColor: '#ebedf0',
-                    borderRadius: '1rem',
-                    color: '#808080',
-                    fontWeight: 'bold',
-                    fontFamily: 'Montserrat, serif',
-                    '&:hover': {
-                        backgroundColor: 'white',
-                        border: '2px solid #5468ff',
-                        color: 'black',
-                    },
-                    [theme.breakpoints.down('sm')]: {
-                        fontSize: '0.8rem',
-                        padding: '0.2rem 0.5rem',
-                    },
-                }}
-            >
-                Fork Me on GitHub
-            </Button>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Button
+                    aria-controls="simple-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                    sx={{
+                        color: 'gray',
+                        display: 'flex',
+                        alignItems: 'center',
+                        '&:hover': {
+                            backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                        },
+                    }}
+                >
+                    Menu
+                    <KeyboardArrowDown sx={{ marginLeft: '2px' }} />
+                </Button>
+                <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+                    <MenuItem onClick={handleClose} component={Link} to="/">
+                        Home
+                    </MenuItem>
+                    <MenuItem onClick={handleClose} component={Link} to="/statistics">
+                        Statistics
+                    </MenuItem>
+                    <MenuItem onClick={handleClose} component={Link} to="/plugin-detail">
+                        Plugin Installation Trend
+                    </MenuItem>
+                    <MenuItem onClick={handleClose} component={Link} to="/">
+                        Plugin Versions by Jenkins Version
+                    </MenuItem>
+                    <MenuItem onClick={handleClose} component={Link} to="/">
+                        Jenkins Plugin Dependency Graph
+                    </MenuItem>
+                    {/* Add more MenuItems as needed */}
+                </Menu>
+                <Button
+                    variant="contained"
+                    startIcon={<FaGithub />}
+                    href="https://github.com/shlomomdahan/stats2.jenkins.io"
+                    sx={{
+                        margin: '0.5rem',
+                        backgroundColor: '#ebedf0',
+                        borderRadius: '1rem',
+                        color: '#808080',
+                        fontWeight: 'bold',
+                        fontFamily: 'Montserrat, serif',
+                        '&:hover': {
+                            backgroundColor: 'white',
+                            border: '2px solid #5468ff',
+                            color: 'black',
+                        },
+                        [theme.breakpoints.down('sm')]: {
+                            fontSize: '0.8rem',
+                            padding: '0.2rem 0.5rem',
+                        },
+                    }}
+                >
+                    Fork Me on GitHub
+                </Button>
+            </Box>
         </StyledBar>
     )
 }
