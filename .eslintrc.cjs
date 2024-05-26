f9095310dcc9f5a2db79593b3d8a9a8620fd8d9c
Kris Stern <krisstern@outlook.com>
2024-05-27 04:48:39 +0800

    fix: fix problem with eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
     rules: {
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'react-refresh/only-export-components': 'off',
         'no-unused-vars': 'error',
         'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
     },
