e78e98bc4c834d6c7dd3465f4c76a5f1308e06f6
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-06-17 19:54:59 -0400

    use jenkins io components header/footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,76 +1,87 @@
-import { Box, Stack, Typography, useMediaQuery } from '@mui/material'
-import ForkMeButton from './Buttons/ForkMeButton'
+import React from 'react'
 
-export default function Footer() {
-    const isMobile = useMediaQuery('(max-width:600px)')
+// Define a wrapper to use the jio-navbar component in JSX
+const JioFooter = () => <jio-footer property="https://contributors.jenkins.io"></jio-footer>
 
-    return (
-        <Stack
-            sx={{
-                width: '100%',
-                height: '10rem',
-                backgroundColor: '#1077ad',
-                justifyContent: 'flex-end',
-                alignItems: 'center',
-                padding: '2rem 0',
-            }}
-        >
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    textAlign: 'center',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100%',
-                }}
-            >
-                {isMobile && (
-                    <Box
-                        sx={{
-                            padding: '1rem',
-                        }}
-                    >
-                        <ForkMeButton />
-                    </Box>
-                )}
-                <Typography
-                    variant="body1"
-                    sx={{
-                        fontFamily: 'Georgia, serif',
-                        color: 'white',
-                        padding: '0.5rem',
-                    }}
-                >
-                    Jenkins-stats maintained by{' '}
-                    <a
-                        href="https://github.com/jenkinsci/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        style={{ color: 'white' }}
-                    >
-                        <strong>community</strong>
-                    </a>
-                </Typography>
-                <Typography
-                    variant="body1"
-                    sx={{
-                        fontFamily: 'Georgia, serif',
-                        color: 'white',
-                        padding: '0.5rem',
-                    }}
-                >
-                    Published with{' '}
-                    <a
-                        href="https://github.com/jenkinsci/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        style={{ color: 'white' }}
-                    >
-                        <strong>GitHub Pages</strong>
-                    </a>
-                </Typography>
-            </Box>
-        </Stack>
-    )
+const Footer: React.FC = () => {
+    return <JioFooter />
 }
+
+export default Footer
+
+// import { Box, Stack, Typography, useMediaQuery } from '@mui/material'
+// import ForkMeButton from './Buttons/ForkMeButton'
+
+// export default function Footer() {
+//     const isMobile = useMediaQuery('(max-width:600px)')
+
+//     return (
+//         <Stack
+//             sx={{
+//                 width: '100%',
+//                 height: '10rem',
+//                 backgroundColor: '#1077ad',
+//                 justifyContent: 'flex-end',
+//                 alignItems: 'center',
+//                 padding: '2rem 0',
+//             }}
+//         >
+//             <Box
+//                 sx={{
+//                     display: 'flex',
+//                     flexDirection: 'column',
+//                     textAlign: 'center',
+//                     justifyContent: 'center',
+//                     alignItems: 'center',
+//                     height: '100%',
+//                 }}
+//             >
+//                 {isMobile && (
+//                     <Box
+//                         sx={{
+//                             padding: '1rem',
+//                         }}
+//                     >
+//                         <ForkMeButton />
+//                     </Box>
+//                 )}
+//                 <Typography
+//                     variant="body1"
+//                     sx={{
+//                         fontFamily: 'Georgia, serif',
+//                         color: 'white',
+//                         padding: '0.5rem',
+//                     }}
+//                 >
+//                     Jenkins-stats maintained by{' '}
+//                     <a
+//                         href="https://github.com/jenkinsci/"
+//                         target="_blank"
+//                         rel="noopener noreferrer"
+//                         style={{ color: 'white' }}
+//                     >
+//                         <strong>community</strong>
+//                     </a>
+//                 </Typography>
+//                 <Typography
+//                     variant="body1"
+//                     sx={{
+//                         fontFamily: 'Georgia, serif',
+//                         color: 'white',
+//                         padding: '0.5rem',
+//                     }}
+//                 >
+//                     Published with{' '}
+//                     <a
+//                         href="https://github.com/jenkinsci/"
+//                         target="_blank"
+//                         rel="noopener noreferrer"
+//                         style={{ color: 'white' }}
+//                     >
+//                         <strong>GitHub Pages</strong>
+//                     </a>
+//                 </Typography>
+//             </Box>
+//         </Stack>
+//     )
+// }
--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Box, Paper } from '@mui/material'
-import NavBar from '../../components/Layout/NavBar'
 import Chart from '../../components/StatsInDetail/Charts/OverallTrendsChart'
 import StatisticsTable from '../../components/StatsInDetail/Charts/StatisticsTable'
 import Sidebar from '../../components/StatsInDetail/Layout/Sidebar'
@@ -21,69 +20,70 @@ const Statistics: React.FC = () => {
     const { selectedChart, selectedTab, selectedYear, handleChartSelect, handleYearSelect } = useSelectionState()
 
     return (
-        <Box
-            className="background"
-            sx={{
-                width: '100vw',
-                height: '100vh',
-                display: 'flex',
-                flexDirection: 'column',
-                backgroundColor: 'white',
-            }}
-        >
-            <NavBar />
+        <>
             <Box
+                className="background"
                 sx={{
+                    width: '100vw',
+                    height: '100vh',
                     display: 'flex',
-                    flexGrow: 1,
-                    overflow: 'hidden',
-                    width: '100%',
-                    flexDirection: 'row',
-                    backgroundColor: '#d5d5d5',
+                    flexDirection: 'column',
+                    backgroundColor: 'white',
                 }}
             >
-                <Sidebar
-                    sidebarOpen={sidebarOpen}
-                    toggleSidebar={toggleSidebar}
-                    selectedChart={selectedChart}
-                    selectedYear={selectedYear}
-                    handleChartSelect={handleChartSelect}
-                    handleYearSelect={handleYearSelect}
-                />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexGrow: 1,
+                        overflow: 'hidden',
+                        width: '100%',
+                        flexDirection: 'row',
+                        backgroundColor: '#d5d5d5',
+                    }}
+                >
+                    <Sidebar
+                        sidebarOpen={sidebarOpen}
+                        toggleSidebar={toggleSidebar}
+                        selectedChart={selectedChart}
+                        selectedYear={selectedYear}
+                        handleChartSelect={handleChartSelect}
+                        handleYearSelect={handleYearSelect}
+                    />
 
-                {selectedTab === 'monthly' && <StatisticsTable year={selectedYear} />}
-                {selectedTab === 'overall' && (
-                    <>
-                        {selectedChart && (
-                            <Paper
-                                elevation={16}
-                                sx={{
-                                    width: '100%',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                    padding: '3rem',
-                                    margin: '2rem',
-                                    backgroundColor: 'white',
-                                    borderRadius: 5,
-                                }}
-                            >
-                                {selectedChart === 'JVMs' ? (
-                                    <JVMChart title={chartTitles[selectedChart]} />
-                                ) : (
-                                    <Chart
-                                        key={`${selectedChart}-${sidebarOpen}`} // Force re-render on sidebar open/close
-                                        csvPath={`https://raw.githubusercontent.com/jenkins-infra/infra-statistics/gh-pages/jenkins-stats/svg/total-${selectedChart}.csv`}
-                                        title={chartTitles[selectedChart]}
-                                    />
-                                )}
-                            </Paper>
-                        )}
-                    </>
-                )}
+                    {selectedTab === 'monthly' && <StatisticsTable year={selectedYear} />}
+                    {selectedTab === 'overall' && (
+                        <>
+                            {selectedChart && (
+                                <Paper
+                                    elevation={16}
+                                    sx={{
+                                        width: '100%',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        padding: '3rem',
+                                        margin: '2rem',
+                                        backgroundColor: 'white',
+                                        borderRadius: 5,
+                                    }}
+                                >
+                                    {selectedChart === 'JVMs' ? (
+                                        <JVMChart title={chartTitles[selectedChart]} />
+                                    ) : (
+                                        <Chart
+                                            key={`${selectedChart}-${sidebarOpen}`} // Force re-render on sidebar open/close
+                                            csvPath={`https://raw.githubusercontent.com/jenkins-infra/infra-statistics/gh-pages/jenkins-stats/svg/total-${selectedChart}.csv`}
+                                            title={chartTitles[selectedChart]}
+                                        />
+                                    )}
+                                </Paper>
+                            )}
+                        </>
+                    )}
+                </Box>
             </Box>
-        </Box>
+        </>
     )
 }
 
