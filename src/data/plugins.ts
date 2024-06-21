cfc68242238599b1dcbc1377c914c98dd06dd894
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-06-21 17:42:12 -0400

    remove comments

--- src/data/plugins.ts
+++ src/data/plugins.ts
@@ -103,10 +103,3 @@ export const pluginList: { id: string }[] = [
     { id: 'agent-maintenance' },
     { id: 'agent-server-parameter' },
 ]
-
-export interface Cell {
-    version: string
-    count: number
-    opacity: number
-    title: string
-}
