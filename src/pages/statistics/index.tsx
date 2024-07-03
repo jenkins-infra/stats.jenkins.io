5b1b18cd886b2cada2b37bc1c667f9da05fc4149
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-07-03 17:26:33 -0400

    test -broken

--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -24,6 +24,7 @@ const Statistics: React.FC = () => {
 
     const csvFileName = `total-${selectedChart}`
     const { data: csvData } = useCSVData(csvFileName)
+    console.log('data for total-', selectedChart, csvData)
 
     return (
         <Stack
