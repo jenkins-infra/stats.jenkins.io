3caa2b7004576255fee7d1a6a8846d1f62b19b12
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-07-03 15:38:32 -0400

    change filepath - test deplyment

--- src/hooks/usePluginCount.ts
+++ src/hooks/usePluginCount.ts
@@ -1,16 +1,13 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import latestNumbers from '../data/infra-statistics/plugin-installation-trend/latestNumbers.json'
 
 const usePluginCount = () => {
     const [pluginCount, setPluginCount] = useState<number>(0)
 
     useEffect(() => {
-        const fetchPluginCount = async () => {
+        const fetchPluginCount = () => {
             try {
-                const url = `https://stats.jenkins.io/plugin-installation-trend/latestNumbers.json`
-                const response = await axios.get(url)
-
-                const plugins = response.data.plugins
+                const plugins = latestNumbers.plugins
                 if (plugins) {
                     const count = Object.keys(plugins).length
                     setPluginCount(count)
@@ -18,7 +15,7 @@ const usePluginCount = () => {
                     console.error('Plugins data not found')
                 }
             } catch (error) {
-                console.error('Error fetching plugin data', error)
+                console.error('Error processing plugin data', error)
             }
         }
         fetchPluginCount()
@@ -28,3 +25,34 @@ const usePluginCount = () => {
 }
 
 export default usePluginCount
+
+// import { useState, useEffect } from 'react'
+// import axios from 'axios'
+
+// const usePluginCount = () => {
+//     const [pluginCount, setPluginCount] = useState<number>(0)
+
+//     useEffect(() => {
+//         const fetchPluginCount = async () => {
+//             try {
+//                 const url = `https://stats.jenkins.io/plugin-installation-trend/latestNumbers.json`
+//                 const response = await axios.get(url)
+
+//                 const plugins = response.data.plugins
+//                 if (plugins) {
+//                     const count = Object.keys(plugins).length
+//                     setPluginCount(count)
+//                 } else {
+//                     console.error('Plugins data not found')
+//                 }
+//             } catch (error) {
+//                 console.error('Error fetching plugin data', error)
+//             }
+//         }
+//         fetchPluginCount()
+//     }, [])
+
+//     return { pluginCount }
+// }
+
+// export default usePluginCount
