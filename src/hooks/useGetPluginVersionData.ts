5cb064bca00d9605e9b033a7ae03be2ef5967ffb
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-07-16 10:29:53 -0400

    fix lint error

--- src/hooks/useGetPluginVersionData.ts
+++ src/hooks/useGetPluginVersionData.ts
@@ -1,14 +1,7 @@
 import { useState, useEffect } from 'react'
-<<<<<<< HEAD
 import { jsonFileMapping } from '../utils/dataLoader'
 import { PluginVersionData, VersionData } from '../types/types'
-=======
-import { PluginVersionData, VersionData } from '../types/types'
-import rawPluginVersionData from '../data/infra-statistics/plugin-installation-trend/jenkins-version-per-plugin-version.json'
 
->>>>>>> 87bf2144f3da362b0e127ab227849e407ca11067
-
-// Casting jsonFileMapping entry to PluginVersionData
 const pluginVersionData = JSON.parse(jsonFileMapping['jenkins-version-per-plugin-version.json']) as PluginVersionData
 
 const useGetPluginVersionData = (pluginId: string | null) => {
