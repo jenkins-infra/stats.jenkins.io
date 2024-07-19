91588aa60d5920ce2473062402dabfd0fbfa992d
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-07-19 11:05:21 -0400

    fix svg render issue

--- src/theme/customTheme.ts
+++ src/theme/customTheme.ts
@@ -1,48 +1,48 @@
 const customTheme = {
     textStyle: {
         fontFamily:
-            'system-ui, "Segoe UI", roboto, "Noto Sans", oxygen, ubuntu, cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+            'system-ui, Segoe UI, roboto, Noto Sans, oxygen, ubuntu, cantarell, Fira Sans, Droid Sans, Helvetica Neue, arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
     },
     title: {
         textStyle: {
             fontFamily:
-                'system-ui, "Segoe UI", roboto, "Noto Sans", oxygen, ubuntu, cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+                'system-ui, Segoe UI, roboto, Noto Sans, oxygen, ubuntu, cantarell, Fira Sans, Droid Sans, Helvetica Neue, arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
         },
         subtextStyle: {
             fontFamily:
-                'system-ui, "Segoe UI", roboto, "Noto Sans", oxygen, ubuntu, cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+                'system-ui, Segoe UI, roboto, Noto Sans, oxygen, ubuntu, cantarell, Fira Sans, Droid Sans, Helvetica Neue, arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
         },
     },
     tooltip: {
         textStyle: {
             fontFamily:
-                'system-ui, "Segoe UI", roboto, "Noto Sans", oxygen, ubuntu, cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+                'system-ui, Segoe UI, roboto, Noto Sans, oxygen, ubuntu, cantarell, Fira Sans, Droid Sans, Helvetica Neue, arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
         },
     },
     legend: {
         textStyle: {
             fontFamily:
-                'system-ui, "Segoe UI", roboto, "Noto Sans", oxygen, ubuntu, cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+                'system-ui, Segoe UI, roboto, Noto Sans, oxygen, ubuntu, cantarell, Fira Sans, Droid Sans, Helvetica Neue, arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
         },
     },
     xAxis: {
         axisLabel: {
             fontFamily:
-                'system-ui, "Segoe UI", roboto, "Noto Sans", oxygen, ubuntu, cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+                'system-ui, Segoe UI, roboto, Noto Sans, oxygen, ubuntu, cantarell, Fira Sans, Droid Sans, Helvetica Neue, arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
         },
         nameTextStyle: {
             fontFamily:
-                'system-ui, "Segoe UI", roboto, "Noto Sans", oxygen, ubuntu, cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+                'system-ui, Segoe UI, roboto, Noto Sans, oxygen, ubuntu, cantarell, Fira Sans, Droid Sans, Helvetica Neue, arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
         },
     },
     yAxis: {
         axisLabel: {
             fontFamily:
-                'system-ui, "Segoe UI", roboto, "Noto Sans", oxygen, ubuntu, cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+                'system-ui, Segoe UI, roboto, Noto Sans, oxygen, ubuntu, cantarell, Fira Sans, Droid Sans, Helvetica Neue, arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
         },
         nameTextStyle: {
             fontFamily:
-                'system-ui, "Segoe UI", roboto, "Noto Sans", oxygen, ubuntu, cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+                'system-ui, Segoe UI, roboto, Noto Sans, oxygen, ubuntu, cantarell, Fira Sans, Droid Sans, Helvetica Neue, arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
         },
     },
 }
