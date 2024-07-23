315eaed64b5d3f282fb949ac0089dd964ca7ad27
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-07-23 16:30:02 -0400

    fetch plguins in batches

--- src/hooks/useFetchPlugins.ts
+++ src/hooks/useFetchPlugins.ts
@@ -2,26 +2,39 @@ import { useState, useEffect } from 'react';
 import { IPluginData } from '../types/types';
 import useGetPluginNames from './useGetPluginNamesAndCount';
 
+const BATCH_SIZE = 50;  
+
 const useFetchPlugins = () => {
     const [plugins, setPlugins] = useState<IPluginData[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const { pluginNames } = useGetPluginNames();
 
     useEffect(() => {
-        const fetchPluginList = async () => {
+        const fetchPluginsInBatch = async (batch: string[]) => {
+            const pluginData = await Promise.all(
+                batch.map(async (name) => {
+                    const fileUrl = new URL(`../data/infra-statistics/plugin-installation-trend/${name}.stats.json`, import.meta.url).href;
+                    const response = await fetch(fileUrl);
+                    if (!response.ok) {
+                        throw new Error(`JSON file for plugin with id "${name}" not found`);
+                    }
+                    const chartData = await response.json();
+                    return { id: name, chartData };
+                })
+            );
+            return pluginData;
+        };
+
+        const fetchAllPlugins = async () => {
+            setLoading(true);
             try {
-                const pluginData = await Promise.all(
-                    pluginNames.map(async (name) => {
-                        const fileUrl = new URL(`../data/infra-statistics/plugin-installation-trend/${name}.stats.json`, import.meta.url).href;
-                        const response = await fetch(fileUrl);
-                        if (!response.ok) {
-                            throw new Error(`JSON file for plugin with id "${name}" not found`);
-                        }
-                        const chartData = await response.json();
-                        return { id: name, chartData };
-                    })
-                );
-                setPlugins(pluginData);
+                let allPlugins: IPluginData[] = [];
+                for (let i = 0; i < pluginNames.length; i += BATCH_SIZE) {
+                    const batch = pluginNames.slice(i, i + BATCH_SIZE);
+                    const batchData = await fetchPluginsInBatch(batch);
+                    allPlugins = [...allPlugins, ...batchData];
+                    setPlugins(allPlugins);
+                }
             } catch (error) {
                 console.error('Error fetching plugin data', error);
             } finally {
@@ -30,7 +43,7 @@ const useFetchPlugins = () => {
         };
 
         if (pluginNames.length > 0) {
-            fetchPluginList();
+            fetchAllPlugins();
         } else {
             setLoading(false);
         }
