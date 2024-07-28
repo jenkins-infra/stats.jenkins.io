c3b6403c457375e0c9a26e13df22e7ef40bc8aba
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-07-28 15:46:32 -0400

    remove comments / gitignore bundle analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import { visualizer } from 'rollup-plugin-visualizer'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [
-//         react(),
-//         visualizer({
-//             open: true,
-//             filename: 'bundle-analysis.html',
-//         }),
-//     ],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
