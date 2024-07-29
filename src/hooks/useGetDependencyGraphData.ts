9d85aad23bb34b4ad0cdc2a95f7e7cc206bbd926
Shlomo Dahan <64103471+shlomomdahan@users.noreply.github.com>
2024-07-29 11:08:57 -0400

    feat: adds dependency graph page (#114)
    * dep graph
    
    * working but not useful
    
    * 3d dep graph
    
    * imporve search / filter
    
    imporve search / filter
    
    imporve search / filter
    
    * remove comments / gitignore bundle analysis
    
    * remove dummy data file blocks.json
    
    ---------
    
    Co-authored-by: Kris Stern <krisstern@outlook.com>

---
+++ src/hooks/useGetDependencyGraphData.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import { PluginData, PluginNode } from '../types/types'
+
+const usePluginData = () => {
+    const [plugins, setPlugins] = useState<PluginNode[]>([])
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        const fetchPlugins = async () => {
+            try {
+                const fileUrl = new URL(`../data/infra-statistics/update-center.actual.json`, import.meta.url).href
+                const response = await fetch(fileUrl)
+                if (!response.ok) {
+                    throw new Error('Network response was not ok')
+                }
+                const data: PluginData = (await response.json()) as PluginData
+
+                const pluginArray = Object.keys(data.plugins).map((key) => {
+                    const plugin = data.plugins[key]
+                    return {
+                        ...plugin,
+                        defaultBranch: plugin.defaultBranch || 'N/A',
+                        scm: plugin.scm || 'N/A',
+                        issueTrackers: plugin.issueTrackers || [],
+                    }
+                })
+
+                setPlugins(pluginArray)
+            } catch (error) {
+                setError((error as Error).message)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchPlugins()
+    }, [])
+
+    return { plugins, loading, error }
+}
+
+export default usePluginData
