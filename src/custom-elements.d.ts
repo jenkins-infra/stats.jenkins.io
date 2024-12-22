96243cb7e5e5a4c5e44e142fd5e47d7a739e21bd
Kris Stern <krisstern@outlook.com>
2024-12-22 23:28:28 +0800

    fix: patch upgrade build issues

--- src/custom-elements.d.ts
+++ src/custom-elements.d.ts
@@ -1,6 +1,10 @@
-declare namespace JSX {
-    interface IntrinsicElements {
-        'jio-navbar': unknown
-        'jio-footer': unknown
+declare global {
+    declare namespace JSX {
+        interface IntrinsicElements {
+            // 'jio-navbar': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+            // 'jio-footer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+            'jio-navbar': unknown;
+            'jio-footer': unknown;
+        }
     }
 }
