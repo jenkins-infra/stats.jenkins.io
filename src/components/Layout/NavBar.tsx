8b461a2e4dfea303323881a6a4681fd7fac37c66
PratikMane0112 <pratikmane610@gmail.com>
2025-12-15 20:10:31 +0530

    src/components/Layout/NavBar.tsx

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const JioNavbar = () => <jio-navbar property="https://contributors.jenkins.io"></jio-navbar>
+const JioNavbar = () => <jio-navbar property="https://stats.jenkins.io"></jio-navbar>
 
 const NavBar: React.FC = () => {
     return <JioNavbar />
