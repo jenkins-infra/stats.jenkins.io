8434a10982b1391937e997bdcad6a850fe3c70a7
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-12-16 22:54:52 +0530

    NavBar Restructured

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,18 +1,4 @@
-// import React from 'react';
 
-// type JioNavbarProps = {
-//   property: string;
-// };
-
-// const JioNavbar: React.FC<JioNavbarProps> = ({ property }) => {
-//   return <div>{property}</div>;
-// };
-
-// const NavBar: React.FC = () => {
-//   return <JioNavbar property="https://contributors.jenkins.io" />;
-// };
-
-// export default NavBar;
 import React from 'react';
 
 type JioNavbarProps = {
